6bf143cc927ce4c1f183415a3425187120e926b1
onlinekkabchi <onlinekkabchi@gmail.com>
2023-06-07 21:04:08 +0900

    update

---
+++ src/app/api/story/route.js
@@ -0,0 +1,26 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export async function GET(req, res) {
+  try {
+    const conn = await client.db("story").collection("short_story");
+    const result = await conn.find({}).limit(10).toArray();
+
+    return new Response(JSON.stringify({ result: result }), {
+      status: 200,
+    });
+  } catch (err) {
+    return new Response({ Error: err }, { status: 500 });
+  } finally {
+    await client.close();
+  }
+}
