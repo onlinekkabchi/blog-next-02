55ffba7077769ee285d509adf8ab526ab944236b
onlinekkabchi <onlinekkabchi@gmail.com>
2023-06-08 10:56:40 +0900

    index

--- src/components/storyBox/storyBox.jsx
+++ src/components/storyBox/storyBox.jsx
@@ -2,10 +2,9 @@
 
 import { useEffect, useState } from "react";
 
-import Story from "./story";
-
 export default async function StoryBox() {
   const [data, setData] = useState([]);
+  const [page, setPage] = useState([0, 1, 2]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,26 +21,37 @@ export default async function StoryBox() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    setPage([1, 2]);
+  }, []);
+
   return (
-    <ul className="story-box">
-      {data.map((item, index) => (
-        <div key={index} className="story--card">
-          <input
-            className="story--input"
-            type="radio"
-            name="story--name"
-            id={item.order}
-          />
-          <label className="story--label" htmlFor={item.order}>
-            <p className="story--title">{item.storyTitle}</p>
-          </label>
-          <div className="story--content">
-            <p className="story--content-text">{item.storyContent}</p>
-            <Tags data={item.storyTag} />
+    <>
+      <div className="story-index-box">
+        {page.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </div>
+      <ul className="story-box">
+        {data.map((item, index) => (
+          <div key={index} className="story--card">
+            <input
+              className="story--input"
+              type="radio"
+              name="story--name"
+              id={item.order}
+            />
+            <label className="story--label" htmlFor={item.order}>
+              <p className="story--title">{item.storyTitle}</p>
+            </label>
+            <div className="story--content">
+              <p className="story--content-text">{item.storyContent}</p>
+              <Tags data={item.storyTag} />
+            </div>
           </div>
-        </div>
-      ))}
-    </ul>
+        ))}
+      </ul>
+    </>
   );
 }
 
