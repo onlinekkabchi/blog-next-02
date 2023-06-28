f128e012e2b3adcd3ea0d3f99f332c7aa6ae343a
onlinekkabchi <onlinekkabchi@gmail.com>
2023-06-28 19:20:46 +0900

    [style] material ui

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,18 @@
+"use client";
+
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 import StoryBox from "../components/storyBox/storyBox";
 
 export default async function Home() {
-  return <StoryBox />;
+  return (
+    <>
+      <Stack direction="column" justifyContent="center" alignItems="center">
+        <ul className="story-box">
+          <StoryBox />
+        </ul>
+        <Pagination count={11} defaultPage={6} />
+      </Stack>
+    </>
+  );
 }
